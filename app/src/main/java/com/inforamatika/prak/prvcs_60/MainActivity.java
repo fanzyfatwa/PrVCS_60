f5f7eb0859e1c992a1e4b0f771b222361213c6cf
fanzyfatwa <zie.jibon@gmail.com>
2017-10-09 16:02:56 +0700

    nambahin kode program di MainActivity dan activit_main.xml

--- app/src/main/java/com/inforamatika/prak/prvcs_60/MainActivity.java
+++ app/src/main/java/com/inforamatika/prak/prvcs_60/MainActivity.java
@@ -2,12 +2,15 @@ package com.inforamatika.prak.prvcs_60;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView teks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teks= (TextView) findViewById(R.id.teks);
     }
+
 }
